b7f1a24ed7d79eae15e401841c6887194687f7e3
riham <riham91.alsiyabi@gmail.com>
2025-04-13 10:28:13 +0400

    // global variables and arraies for flight .......

--- AirlineReservationConsoleSystem_1/Program.cs
+++ AirlineReservationConsoleSystem_1/Program.cs
@@ -2,9 +2,73 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+           // global variables and arraies for flight ....... 
+        static int Max_Flight = 3;
+        static int FlightCounter = 0;
+        static string[] flightCode_A = new string[Max_Flight];
+        static string[] fromCity_A = new string[Max_Flight];
+        static string[] toCity_A = new string[Max_Flight];
+        static DateTime[] departureTime_A = new DateTime[Max_Flight];
+        static int[] duration_A = new int[Max_Flight];
+        static int[] SeatsNum_A = new int[Max_Flight];
+        static int[] SeatReserved_A = new int[Max_Flight];
+
+        // flag to validate the user input 
+        static bool isValid = false;
+
+        //                                =====================Startup & Navigation=============
+
+        // 1. display welcome message method..........
+        public static void DisplayWelcomeMessage()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Airline Reservation System");
+        }
+        // 2. show main menu method
+        public static int ShowMainMenu()
+        {
+            int option = 0;
+            do
+            {
+                // Just to clear the screen
+                Console.Clear();
+                //Print the menu lists
+                Console.WriteLine("Airline Reservation System");
+                Console.WriteLine("1. Add Flight");
+                Console.WriteLine("2. Display All Flights");
+                Console.WriteLine("3. Find Flight By Code");
+                Console.WriteLine("4. Update Flight Departure");
+                Console.WriteLine("5. Cancel Flight Booking");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("Enter the option: ");
+                string input = Console.ReadLine();
+                try
+                {
+                    option = int.Parse(input);// Attempt to parse the input
+                    isValid = true; // If parsing is successful, set isValid to true
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a number between 0 and 5.");
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey(); // Wait for user to acknowledge the message
+                    isValid = false; // Keep isValid false to repeat the loop
+                }
+            } while (!isValid); // Continue looping until valid input is received
+
+            return option;
+        }
+
+        // 3. Exit Application method
+        public static void ExitApplication()
+        {
+            Console.WriteLine("Thanks! Have Happy Day!");
         }
     }
+
+
+
+
+
+
+    
 }
