00fc65053706603628691d04cad9b11a9533cf37
riham <riham91.alsiyabi@gmail.com>
2025-04-14 09:08:06 +0400

    // Wait for user to press any key before continuing

--- AirlineReservationConsoleSystem_1/Program.cs
+++ AirlineReservationConsoleSystem_1/Program.cs
@@ -31,7 +31,7 @@
 
         //   =====================Startup & Navigation=============
 
-        // 1. display welcome message method......
+        // 1. display welcome message method.....
         public static void WelcomeMessage()
         {
             Console.WriteLine("Welcome to Airline Reservation System");
